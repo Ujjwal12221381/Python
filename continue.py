acfae144c56d218fdfc24ff3ef19a8628b03ece3
Ujjwal12221381 <119718279+Ujjwal12221381@users.noreply.github.com>
2022-12-13 18:01:36 +0530

    Add files via upload

---
+++ continue.py
@@ -0,0 +1,19 @@
+#continue
+x=int(input())
+y=x + (x-1)-1
+mid=int(y/2)
+index=mid
+for i in range(x):
+    if i %2!=0 :
+        continue
+    else:
+        print(" "*index, end="")
+        for j in range (index,mid+1):
+            if j%2==0:
+                print("x", end="")
+            else:
+                print(" ", end="")
+        index -=2
+        mid +=2
+        print()
+
